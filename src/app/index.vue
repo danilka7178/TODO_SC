f397323edd488dd78e20b3431905e2c1da137754
danilka7178 <danya.sidorow@yandex.ru>
2024-01-27 18:54:01 +0300

    Create vue app

--- src/app/index.vue
+++ src/app/index.vue
@@ -3,5 +3,5 @@ import { Typography } from '@/shared/typography';
 </script>
 
 <template>
-  <Typography tagName="h1" bold size="s">ToDo List Scloud</Typography>
+  <Typography tagName="h1" bold size="s">ToDo List Sclou1d</Typography>
 </template>
