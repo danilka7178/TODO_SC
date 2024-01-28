5c2d28a29df1788a2ed8bff6c31484089ff7faf5
danilka7178 <danya.sidorow@yandex.ru>
2024-01-28 13:45:04 +0300

    add container, icon components

---
+++ src/shared/container/ui.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div class="container"><slot></slot></div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  max-width: 1202px;
+  width: 100%;
+  margin: 0 auto;
+}
+
+@media screen and (max-width: 992px) {
+  .container {
+    max-width: 640px;
+  }
+}
+
+@media screen and (max-width: 640px) {
+  .container {
+    max-width: 295px;
+  }
+}
+</style>
