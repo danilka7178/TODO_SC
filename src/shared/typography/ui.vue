5d753d4710aced4bf6b7aaf34e44a224c2edfd03
danilka7178 <danya.sidorow@yandex.ru>
2024-01-28 13:45:37 +0300

    change Typography component

--- src/shared/typography/ui.vue
+++ src/shared/typography/ui.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 interface Props {
-  tagName: 'span' | 'p' | 'h1';
+  tagName: 'span' | 'p' | 'h1'| 'h2'| 'h3';
   bold?: boolean;
   size?: 'xs' | 's' | 'm' | 'l' | 'xl';
 }
@@ -17,12 +17,14 @@ const props = defineProps<Props>();
   </component>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
   .typography {
     font-family: 'PT Sans Caption', 'serif';
     font-style: normal;
     line-height: 150%;
     color: var(--color-black);
+    margin-block-start: 0;
+    margin-block-end: 0;
   }
 
   .bold { font-weight: 700; }
@@ -31,11 +33,18 @@ const props = defineProps<Props>();
   .size_s { font-size: 14px; }
   .size_m { font-size: 16px; }
   .size_l { font-size: 20px; }
-  .size_xl { font-size: 24px; }
 
   .tag_h1 {
     font-size: 32px;
     font-family: 'Montserrat', 'serif';
+  }
+
+  .tag_h2 {
+    font-size: 24px;
+  }
+
+  .tag_h3 {
+    font-size: 18px;
   }
 
   @media screen and (max-width: 992px) {
@@ -44,11 +53,19 @@ const props = defineProps<Props>();
     .tag_h1 {
       font-size: 24px;
     }
+
+    .tag_h2 {
+      font-size: 18px;
+    }
   }
 
   @media screen and (max-width: 640px) {
 
     .tag_h1 {
+      font-size: 20px;
+    }
+
+    .tag_h3 {
       font-size: 20px;
     }
   }
